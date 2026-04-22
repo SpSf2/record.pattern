4638715c33df568888e67b227ed400887935c0d1
SpSf2 <kiworks.duglas@gmail.com>
2026-04-22 13:36:17 +0200

    sin record pattern

--- record-pattern/src/main/java/com/example/App.java
+++ record-pattern/src/main/java/com/example/App.java
@@ -1,10 +1,27 @@
 package com.example;
 
-/**
- * Hello world!
- */
+	/*  Metodo que recibe un objeto como parametro y muestra los detalles de dicho objeto
+	 * utilizando la sentencia if y Pattern Machine con el metodo instanceof y además destructure
+	 * del tipo record que es uno de los record pattern  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+	
+    static void imprimeDetalles(Object obj) {
+    	
+    	if (obj instanceof Book book)  {
+    		System.out.println("Titulo del Libro: " + book.title());
+    		System.out.println("Autor del libro: " + book.author());
+    		
+    	}
+    }
+    
+    public static void main(String[] args)  {
+    	// Crear un objeto de tipo record Book
+    	
+    	Book libro = Book.builder()
+    			.title("Java es Bueno")
+    			.author("Yo mismo")
+    			.build();
+    	
+    	imprimeDetalles(libro);
     }
 }
